f3ad3f7cd9639b6a20c4fc85ee609c07888e4fb5
jelelahmed <abduljelelahmed>
2023-01-15 00:53:59 +0100

    adding starter files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+				<h2>Smart Insurance</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
